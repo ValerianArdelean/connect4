c15b32e58976b641c2fdb7da94716991bc01321a
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-03-07 19:42:36 +0200

    Implemented the colored cells at click

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let pen = "";
+function buttons(color) {
+    pen = color;
+}
+
 let columns = [0, 0, 0, 0, 0, 0, 0, 0];
 function col(id) {
     id = parseInt(id);
     columns[id] += 1;
     let col = document.getElementById(id);
-    col.children[6 - columns[id]].style.backgroundColor = "gray";
+    col.children[6 - columns[id]].style.backgroundColor = pen;
     console.log(id, columns[id], col.children[6 - columns[id]]);
 }
