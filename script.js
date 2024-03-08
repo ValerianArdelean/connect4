d433dfacb9160846c2607ac7c01ae20d55febe69
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-03-09 01:12:16 +0200

    Fixing a bug with secondary upper paralels

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function checkWinner2(color) {
                 (i <= 2 && mt[j + i][j] == color && mt[j + i + 1][j + 1] == color && mt[j + i + 2][j + 2] == color && mt[j + i + 3][j + 3] == color)) {
                 return true;
             }
-            if ((i <= 4 && j <= 3 && mt[j][(7 - j + 1) - i + 1] == color && mt[j + 1][(7 - j + 1) - i + 1 - 1] == color && mt[j + 2][(7 - j + 1) - i + 1 - 2] == color && mt[j + 3][(7 - j + 1) - i + 1 - 3] == color) ||
+            if ((i <= 4 && mt[j][(7 - j + 1) - i + 1] == color && mt[j + 1][(7 - j + 1) - i + 1 - 1] == color && mt[j + 2][(7 - j + 1) - i + 1 - 2] == color && mt[j + 3][(7 - j + 1) - i + 1 - 3] == color) ||
                 (i <= 2 && mt[j + i][(7 - j + 1)] == color && mt[j + i + 1][(7 - j + 1) - 1] == color && mt[j + i + 2][(7 - j + 1) - 2] == color && mt[j + i + 3][(7 - j + 1) - 3] == color)) {
                 return true;
             }
