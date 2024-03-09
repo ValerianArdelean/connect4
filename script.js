45307a5001420c86dd5a50ebcb8d0e2c163c6e00
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-03-09 19:39:40 +0200

    Mooving constants in to a single line each

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const LINES_NO = 6, COLUMNS_NO = 7, FOUR = 4, THREE = 3, TWO = 2, ONE = 1;
+const LINES_NO = 6;
+const COLUMNS_NO = 7;
+const FOUR = 4;
+const THREE = 3;
+const TWO = 2;
+const ONE = 1;
+
+let mt = [[], [], [], [], [], [], [], []];
+for (let i = 0; i <= COLUMNS_NO; ++i) {
+    mt[i] = [];
+}
 
 let game = {
     columns: [0, 0, 0, 0, 0, 0, 0, 0],
@@ -11,40 +21,35 @@ let game = {
     }
 }
 
-let mt = [[], [], [], [], [], [], [], []];
-for (let i = 0; i <= COLUMNS_NO; ++i) {
-    mt[i] = [];
-}
-
 function checkWinner2(color) {
     for (let i = 1; i <= COLUMNS_NO; ++i) {
         for (let j = 1; j <= FOUR; ++j) {
-            if ((i <= LINES_NO &&
+            if ((i <= LINES_NO &&//checking lines and columns
                  mt[i][j] == color && mt[i][j + 1] == color &&
                  mt[i][j + TWO] == color && mt[i][j + THREE] == color) ||
                  (j <= THREE &&
                  mt[j][i]) == color && mt[j + 1][i] == color &&
                  mt[j + TWO][i] == color && mt[j + THREE][i] == color) {
                     return true;
-            }
+            }//checking paralels above main diagonal
             if ((i <= FOUR && j <= THREE &&
                  mt[j][j + i - 1] == color &&
                  mt[j + 1][j + 1 + i - 1] == color &&
                  mt[j + TWO][j + TWO + i - 1] == color &&
                  mt[j + THREE][j + THREE + i - 1] == color) ||
-                (i <= TWO && j <= TWO &&
+                (i <= TWO && j <= TWO &&//checking paralels bellow main diagonal
                  mt[j + i][j] == color &&
                  mt[j + i + 1][j + 1] == color &&
                  mt[j + i + TWO][j + TWO] == color &&
                  mt[j + i + THREE][j + THREE] == color)) {
                     return true;
-            }
+            }//checking paralels above second diagonal
             if ((i <= FOUR && j <= THREE &&
                  mt[j][(COLUMNS_NO - j + 1) - i + 1] == color &&
                  mt[j + 1][(COLUMNS_NO - j + 1) - i + 1 - 1] == color &&
                  mt[j + TWO][(COLUMNS_NO - j + 1) - i + 1 - 2] == color &&
                  mt[j + THREE][(COLUMNS_NO - j + 1) - i + 1 - THREE] == color) ||
-                (i <= TWO && j <= TWO &&
+                (i <= TWO && j <= TWO &&//checking paralels bellow second diagonal
                  mt[j + i][(COLUMNS_NO - j + 1)] == color &&
                  mt[j + i + 1][(COLUMNS_NO - j + 1) - 1] == color &&
                  mt[j + i + TWO][(COLUMNS_NO - j + 1) - TWO] == color &&
